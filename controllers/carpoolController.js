abf0122dee39bd85a6201c121dd2e96b8ab768b7
Sajal Gupta <guptasajal411@gmail.com>
2021-10-30 08:32:33 +0530

    feat: :rocket: Add available carpools to frontend

--- controllers/carpoolController.js
+++ controllers/carpoolController.js
@@ -1,16 +1,17 @@
 const User = require('../models/userModel');
 
 exports.postNewCarpool = function(req, res){
-    User.findOne({ username: req.body.username }, function(err, foundUser){
+    User.findOne({ username: req.body.username }, async function(err, foundUser){
         if(err){
             res.send(err);
         } else {
             foundUser.carMembers.push({ username: req.body.username });
+            await foundUser.save();
             User.find({}, function(err, users){
                 if(err) {
                     res.send(err);
                 } else {
-                    res.render("carpool", { user: foundUser, allUsers: users });
+                    res.redirect("/");
                 }
             });
         }
